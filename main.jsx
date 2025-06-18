c7e7285f862bc72378a12dde07b88f090e54ff62
NNoradrenaline <koda60608@gmail.com>
2025-06-17 17:04:11 -0700

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+export default function App() {
+  return (
+    <div style={{ backgroundColor: 'white', color: 'black', padding: '2rem' }}>
+      <h1>Nor Website is working!</h1>
+      <p>If you see this, React and Vercel are set up correctly.</p>
+    </div>
+  )
+}
